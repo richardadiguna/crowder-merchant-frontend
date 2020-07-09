4bd5f1dd71e8173c4e24ebe9c6ed302308e22eca
Mayu Krisnawan <mayu.krisnawan@dana.id>
2020-07-09 16:32:21 +0800

    feat: f-icon

---
+++ components/fiat/f-icon/f-icon.js
@@ -0,0 +1,13 @@
+import FiatComponent from '../core/fiat-component'
+
+Component(FiatComponent({
+  mixins: [],
+  data: {},
+  props: {
+    name: '',
+  },
+  didMount() {},
+  didUpdate() {},
+  didUnmount() {},
+  methods: {},
+}));
