5a317a68fa8e495724b196703cec40b8c847e7b6
Mayu Krisnawan <mayu.krisnawan@dana.id>
2020-07-10 12:59:25 +0800

    feat: f-divider

---
+++ components/fiat/f-divider/f-divider.js
@@ -0,0 +1,14 @@
+import FiatComponent from '../core/fiat-component'
+
+Component(FiatComponent({
+  mixins: [],
+  data: {},
+  props: {
+    title: null,
+    type: 'line-full'
+  },
+  didMount() {},
+  didUpdate() {},
+  didUnmount() {},
+  methods: {},
+}));
