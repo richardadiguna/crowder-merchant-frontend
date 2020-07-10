30f0bed161d44b3e13f17b86d7e6d2182fb5ae15
Mayu Krisnawan <mayu.krisnawan@dana.id>
2020-07-10 13:42:34 +0800

    feat: fiat - core - props validation

--- components/fiat/f-divider/f-divider.js
+++ components/fiat/f-divider/f-divider.js
@@ -7,6 +7,16 @@ Component(FiatComponent({
     title: null,
     type: 'line-full'
   },
+  validateProps: {
+    type: (val) => [
+      'box-4',
+      'box-8',
+      'box-12',
+      'box-16',
+      'line-full',
+      'title',
+    ].includes(val),
+  },
   didMount() {},
   didUpdate() {},
   didUnmount() {},
