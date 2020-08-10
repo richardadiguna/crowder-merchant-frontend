54e75ca17922eb08901f224aceae01c7a27eecfc
Mayu Krisnawan <mayu.krisnawan@dana.id>
2020-08-10 15:57:57 +0800

    refactor: remove lodash from depedencies

--- components/denom-list/denom-list.js
+++ components/denom-list/denom-list.js
@@ -1,5 +1,3 @@
-import chunk from 'lodash/chunk'
-
 Component({
   data: {
     denomRows: [],
@@ -18,7 +16,20 @@ Component({
     updateDenomRows(props) {
       if (!props) props = this.props
       const { denoms=[] } = props
-      const denomRows = chunk(denoms, 2)
+      const denomRows = []
+      let denomRow = []
+      let i = 0
+      while (i < denoms.length) {
+        if (i % 2 == 0) {
+          denomRow = [denoms[i]]
+          if (i == denoms.length - 1) denomRows.push(denomRow)
+        } else {
+          denomRow.push(denoms[i])
+          denomRows.push(denomRow)
+          denomRow = []
+        }
+        i++
+      }
       this.setData({ denomRows })
     },
   },
