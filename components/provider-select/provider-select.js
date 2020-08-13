97479665e1e80905d7bb62e5c67254551ed50e27
Mayu Krisnawan <mayu.krisnawan@dana.id>
2020-08-13 19:39:53 +0800

    feat: provider select - layout

---
+++ components/provider-select/provider-select.js
@@ -0,0 +1,14 @@
+Component({
+  mixins: [],
+  data: {},
+  props: {
+    title: "PROVIDER NAME",
+    provider: {
+      name: "Provider Name",
+    },
+  },
+  didMount() {},
+  didUpdate() {},
+  didUnmount() {},
+  methods: {},
+});
